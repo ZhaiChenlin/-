af8c5e31a659644bc3b83338936d7a064bbbc4d7
翟晨林 <454511948@qq.com>
2016-06-29 16:55:03 +0800

    Initial Commit

---
+++ 何末之/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  何末之
+//
+//  Created by Mac on 16/6/29.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
